071c1d9002d5b8474587371d5afbedab5316be0b
Lawrence Zeng <lawrence.zeng@gmail.com>
2020-05-30 12:06:02 -0400

    is a solution

--- src/main/java/game/Sudoku.java
+++ src/main/java/game/Sudoku.java
@@ -12,33 +12,32 @@ import algo.Backtrack;
 public final class Sudoku extends Backtrack<SudokuBoard>{
 
 	@Override
-	protected void unmakeMove(int[] a, int k, SudokuBoard input) {
+	protected void unmakeMove(int[] a, int k, SudokuBoard board) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	protected void makeMove(int[] a, int k, SudokuBoard input) {
+	protected void makeMove(int[] a, int k, SudokuBoard board) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	protected int[] constructCandidates(int[] a, int k, SudokuBoard input) {
+	protected int[] constructCandidates(int[] a, int k, SudokuBoard board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected void processSolution(int[] a, int k, SudokuBoard input) {
+	protected void processSolution(int[] a, int k, SudokuBoard board) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	protected boolean isASolution(int[] a, int k, SudokuBoard input) {
-		// TODO Auto-generated method stub
-		return false;
+	protected boolean isASolution(int[] a, int k, SudokuBoard board) {
+		return board.isComplete();
 	}
 
 }
