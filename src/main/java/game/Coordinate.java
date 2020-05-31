f99871df8c06e6cdd26dc8733ef65bc145f01f53
Lawrence Zeng <lawrence.zeng@gmail.com>
2020-05-31 10:44:16 -0400

    add equals/hash code methods

--- src/main/java/game/Coordinate.java
+++ src/main/java/game/Coordinate.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Objects;
+
 final class Coordinate {
 	
 	private final int x;
@@ -16,6 +18,23 @@ final class Coordinate {
 
 	int y() {
 		return y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinate other = (Coordinate) obj;
+		return x == other.x && y == other.y;
 	}
 	
 	
